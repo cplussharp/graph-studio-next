540698731ecdeff583d3dd5cb3eb3fbbf596873d
mike.copperwhite@gmail.com <mike.copperwhite@gmail.com@c2f33eb2-41b2-391d-4cb4-d9d0608729ba>
2013-03-06 10:45:56 +0000

    Added help menu information for mouse operations and drag and drop (previously was information on command line options)

--- trunk/src/CliOptionsForm.cpp
+++ trunk/src/CliOptionsForm.cpp
@@ -52,7 +52,7 @@ BOOL CCliOptionsForm::OnInitDialog()
 	edit_options.SetFont(&font_options);
 
     CString strTxt;
-    strTxt =  _T("Command Line Options for the programm:\r\n");
+    strTxt =  _T("Command Line Options for the Program:\r\n");
     strTxt += _T("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n\r\n");
     strTxt += _T("graphstudionext.exe [file] [options]\r\n");
     strTxt += _T("-> file (optional)      -> a graph file or a video-file to render\r\n");
@@ -67,7 +67,37 @@ BOOL CCliOptionsForm::OnInitDialog()
     strTxt += _T("    -?                      -> opens this window on startup\r\n");
     strTxt += _T("\r\n");
     strTxt += _T("Example:\r\n");
-    strTxt += _T("graphstudionext.exe convert.grf -run -exitonerror -exitafterrun -noclock -progressview\r\n");
+    strTxt += _T("graphstudionext.exe convert.grf -run -exitonerror -exitafterrun -noclock -progressview\r\n\r\n");
+
+	strTxt += _T("Mouse Buttons:\r\n");
+    strTxt += _T("~~~~~~~~~~~~~~~\r\n\r\n");
+    strTxt += _T("Click                       -> Select filter or connection\r\n");
+    strTxt += _T("Ctrl + Click                -> Toggle selection of clicked filter\r\n");
+    strTxt += _T("Double click                -> filter or pin properties\r\n");
+    strTxt += _T("Right click                 -> filter, pin or graph context menu\r\n");
+    strTxt += _T("Middle button               -> Delete filter or connection\r\n");
+    strTxt += _T("Shift + click + drag        -> Connect from first unconnected output pin\r\n");
+    strTxt += _T("Shift + right click         -> Context menu for first unconnected output pin\r\n");
+    strTxt += _T("Shift + middle click        -> Disconnect first connected output pin on filter\r\n\r\n");
+
+    strTxt += _T("Drag and Drop of File(s):\r\n");
+    strTxt += _T("~~~~~~~~~~~~~~~~~~~~~~~~~~\r\n\r\n");
+    strTxt += _T("Drop files                  -> Open .GRF/.XML graph file or render other media file(s)\r\n");
+    strTxt += _T("Shift + drop files          -> Open file(s) with Async File Reader filter\r\n");
+    strTxt += _T("Alt + drop files            -> Create source filter for file(s) with IGraphBuilder::AddSourceFilter\r\n");
+    strTxt += _T("Control + any above         -> Do not clear existing graph before adding file(s)\r\n");
+    strTxt += _T("Drop files on filter        -> Set filename for IFileSourceFilter or IFileSink if supported\r\n\r\n");
+
+    strTxt += _T("Mouse Wheel:\r\n");
+    strTxt += _T("~~~~~~~~~~~~~\r\n\r\n");
+    strTxt += _T("Wheel                       -> Scroll graph\r\n");
+    strTxt += _T("Horizontal wheel            -> Scroll graph horizontally\r\n");
+    strTxt += _T("Alt + wheel                 -> Scroll graph horizontally\r\n");
+    strTxt += _T("Ctrl + wheel                -> Change zoom level\r\n");
+    strTxt += _T("Shift + wheel               -> Change vertical filter spacing\r\n");
+    strTxt += _T("Shift + horizontal wheel    -> Change horizontal filter spacing\r\n");
+    strTxt += _T("Alt + Shift + wheel         -> Change horizontal filter spacing\r\n");
+    strTxt += _T("Ctrl + Shift + wheel        -> Change playback speed\r\n\r\n");
 
     edit_options.SetWindowText(strTxt);
 
