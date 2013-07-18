48505e92d1039a3405b4aaeac57d317d955d44b0
mike.copperwhite@gmail.com <mike.copperwhite@gmail.com@c2f33eb2-41b2-391d-4cb4-d9d0608729ba>
2013-07-18 12:12:00 +0000

    Fix issue #170 : Tooltips - hide tooltip window if no text to display.

--- trunk/src/CustomToolTipCtrl.cpp
+++ trunk/src/CustomToolTipCtrl.cpp
@@ -2,6 +2,13 @@
 #include "CustomToolTipCtrl.h"
 #include "MainFrm.h"
 
+
+BEGIN_MESSAGE_MAP(CCustomToolTipCtrl, CMFCToolTipCtrl)
+	//{{AFX_MSG_MAP(CMFCToolTipCtrl)
+	ON_NOTIFY_REFLECT(TTN_SHOW, &CCustomToolTipCtrl::OnShow)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
 /////////////////////////////////////////////////////////////////////////////
 // CCustomToolTipCtrl
 CCustomToolTipCtrl::CCustomToolTipCtrl(CustomToolTipCallback* callback)
@@ -23,19 +30,12 @@ CSize CCustomToolTipCtrl::OnDrawLabel(CDC* pDC, CRect rect, BOOL bCalcOnly)
 	POINT cursor;
 	GetCursorPos(&cursor);
 
-	CString labelText, descriptionText;
-	if (m_callback) {
-		m_callback->GetToolTipLabelText(cursor, labelText, descriptionText);
-	}
-
 	CSize sizeText(0, 0);
-	if (labelText.GetLength() == 0 && descriptionText.GetLength() == 0) {
-		SetDescription(CString());
-	} else {
+	if (label.GetLength() > 0 || m_strDescription.GetLength() > 0) {
 		const BOOL bDrawDescr = m_Params.m_bDrawDescription && !m_strDescription.IsEmpty();
 		CFont* const pOldFont = (CFont*) pDC->SelectObject(m_Params.m_bBoldLabel && bDrawDescr ? &afxGlobalData.fontBold : &afxGlobalData.fontTooltip);
 
-		if (labelText.Find(_T('\n')) >= 0) // Multi-line text
+		if (label.Find(_T('\n')) >= 0) // Multi-line text
 		{
 			UINT nFormat = DT_NOPREFIX;
 			if (bCalcOnly)
@@ -44,13 +44,13 @@ CSize CCustomToolTipCtrl::OnDrawLabel(CDC* pDC, CRect rect, BOOL bCalcOnly)
 			if (m_pRibbonButton != NULL)
 				nFormat |= DT_NOPREFIX;
 
-			const int nHeight = pDC->DrawText(labelText, rect, nFormat);
+			const int nHeight = pDC->DrawText(label, rect, nFormat);
 			sizeText = CSize(rect.Width(), nHeight);
 		}
 		else
 		{
 			if (bCalcOnly)
-				sizeText = pDC->GetTextExtent(labelText);
+				sizeText = pDC->GetTextExtent(label);
 			else
 			{
 				UINT nFormat = DT_LEFT | DT_NOCLIP | DT_SINGLELINE;
@@ -65,13 +65,34 @@ CSize CCustomToolTipCtrl::OnDrawLabel(CDC* pDC, CRect rect, BOOL bCalcOnly)
 					nFormat |= DT_NOPREFIX;
 				}
 
-				sizeText.cy = pDC->DrawText(labelText, rect, nFormat);
+				sizeText.cy = pDC->DrawText(label, rect, nFormat);
 				sizeText.cx = rect.Width();
 			}
 		}
 		pDC->SelectObject(pOldFont);
-		SetDescription(descriptionText);
+	}
+	return sizeText;
+}
+
+afx_msg void CCustomToolTipCtrl::OnShow(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	POINT cursor;
+	GetCursorPos(&cursor);
+
+	if (m_callback) {
+		label = _T("");
+		CString description;
+		m_callback->GetToolTipLabelText(cursor, label, description);
+		SetDescription(description);
+
+		// if text to display call base class to continue normal display
+		if (label.GetLength() > 0 || m_strDescription.GetLength() > 0) {
+			__super::OnShow(pNMHDR, pResult);
+			return;
+		}
 	}
 
-	return sizeText;
+	// nothing to display, hide tooltip window, resize to zero and return TRUE
+	SetWindowPos(NULL, 0, 0, 0, 0, SWP_HIDEWINDOW | SWP_NOZORDER | SWP_NOACTIVATE);
+	*pResult = TRUE;
 }
--- trunk/src/CustomToolTipCtrl.h
+++ trunk/src/CustomToolTipCtrl.h
@@ -19,5 +19,13 @@ protected:
 	CustomToolTipCallback* m_callback;
 	int	m_nCurrID;
 
+	// Data to display - set in OnShow and cache until used by OnDrawLabel
+	CString label;
+
+	// CMFCToolTipCtrl overrides
 	CSize OnDrawLabel(CDC* pDC,CRect rect,BOOL bCalcOnly);
+
+	afx_msg void OnShow(NMHDR* pNMHDR, LRESULT* pResult);
+
+	DECLARE_MESSAGE_MAP()
 };
