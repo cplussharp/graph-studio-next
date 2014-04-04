bfd0e6f3482b9a6e9910eee4a508ea5d6b64780a
mike.copperwhite@gmail.com <mike.copperwhite@gmail.com@c2f33eb2-41b2-391d-4cb4-d9d0608729ba>
2014-04-04 17:49:38 +0000

    Fix issue #168 : Added support for switching between property page tabs with control-tab or control-shift_tab

--- src/PropertyForm.cpp
+++ src/PropertyForm.cpp
@@ -768,5 +768,26 @@ int CPageSite::Activate(HWND owner, CRect &rc)
 	return 0;
 }
 
+BOOL CPropertyForm::PreTranslateMessage(MSG *pMsg)
+{
+	if (pMsg && pMsg->message == WM_KEYDOWN) {
+		if (pMsg->wParam == VK_TAB && container) {
+			if ((0x8000 & GetKeyState(VK_CONTROL)) && !(0x8000 & GetKeyState(VK_MENU))) {	// control-tab to change page
+				const int numPages = container->pages.GetCount();
+				if (numPages > 1) {
+					const int increment = (0x8000 & GetKeyState(VK_SHIFT)) ? -1 : 1;		// added shift iterates backwards
+					int newPage = tabs.GetCurSel() + increment;
 
+					if (newPage < 0)
+						newPage = numPages - 1;
+					else if (newPage >= numPages)
+						newPage = 0;
+					container->ActivatePage(newPage);
+				}
+				return TRUE;
+			}
+		}
+	}
 
+	return __super::PreTranslateMessage(pMsg);
+}
