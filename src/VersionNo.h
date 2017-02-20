49101f069d45608d9afafc2432e0b3a2915eff75
Mike Copperwhite <mikecopperwhite@users.noreply.github.com>
2017-02-20 20:19:45 +0000

    Update issue #299 : In Visual Studio 2013 and 2015 project Pass through following Apveyor environment variables as C++ preprocessor and resource compile variables APPVEYOR_BUILD_VERSION APPVEYOR_BUILD_NUMBER Added display of Appveyor build version information in about box as a test.

--- src/VersionNo.h
+++ src/VersionNo.h
@@ -10,16 +10,19 @@
 
 #define VERSION_MODIFIER
  
-#define VER_FILE_DESCRIPTION_STR    "Built " __DATE__ " " __TIME__
+// If built on appveyor, pass build version information through
+//#define APPVEYOR_BUILD_VERSION 98.8.34.1345
+#if defined(APPVEYOR_BUILD_VERSION) && defined(APPVEYOR_BUILD_NUMBER)
+#define APPVEYOR_BUILD_STRING "Appveyor " STRINGIZE(APPVEYOR_BUILD_VERSION) " : " STRINGIZE(APPVEYOR_BUILD_NUMBER)
+#else
+#define APPVEYOR_BUILD_STRING ""
+#endif
+
+#define VER_FILE_DESCRIPTION_STR    "Built " __DATE__ " " __TIME__ " " APPVEYOR_BUILD_STRING
+
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
 #define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR)        \
                                     "." STRINGIZE(VERSION_MINOR)    \
                                     "." STRINGIZE(VERSION_REVISION) \
                                     "." STRINGIZE(VERSION_BUILD)
-
-#define VER_FILE_VERSION_STR2       STRINGIZE(VERSION_MAJOR)        \
-                                    "." STRINGIZE(VERSION_MINOR)    \
-                                    "." STRINGIZE(VERSION_REVISION) \
-                                    "." STRINGIZE(VERSION_BUILD)    \
-                                    VERSION_MODIFIER
 
